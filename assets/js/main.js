76e2a9da4c83dabad6bc3ed6ae3b506cb8cd87bc
Abu Hanife Nayem <ahnayem.sust@gmail.com>
2017-06-04 15:49:36 +0600

    animation complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,17 +10,64 @@ $(document).ready(function(){
         nav: true,
         autoHeight: true
     });
-        /*$("#blur").hover().addClass("bounceIn");*/
 
-    $('#blur').on('mouseover', function () {
-        var $this = $(this);
+/*    $(".project-a-11").mouseenter( function () {
+        var $this = $("#blur11");
         var effect = 'zoomIn ';
 
         $this
-            .addClass('animated ' + effect)
+            .addClass('animated blur-show ' + effect)
             .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $this.removeClass('animated ' + effect);
         });
+    });
+
+    $(".project-a-11").mouseleave( function (){
+        $("#blur11").removeClass("blur-show");
+    });*/
+
+/*    for(i=1;i<=11;i++){
+        var hoverClass = "#blur-animated-" + i;
+        var blurClass = "#blur" + i;
+        
+
+        $(hoverClass).mouseenter( function () {
+            var $this = $(blurClass);
+            var effect = 'zoomIn ';
+
+            $this
+                .addClass('animated blur-show ' + effect)
+                .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                $this.removeClass('animated ' + effect);
+            });
+        });
+
+        $(hoverClass).mouseleave( function (){
+            $(blurClass).removeClass("blur-show");
+        });
+    }*/
+
+    $.each([ 1, 2, 3, 4, 5, 6, 7, 8,9,10,11], function( index, value ) {
+      
+        var hoverClass = "#blur-animated-" + value;
+        var blurClass = "#blur" + value;
+        
+
+        $(hoverClass).mouseenter( function () {
+            var $this = $(blurClass);
+            var effect = 'zoomIn ';
+
+            $this
+                .addClass('animated blur-show ' + effect)
+                .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                $this.removeClass('animated ' + effect);
+            });
+        });
+
+        $(hoverClass).mouseleave( function (){
+            $(blurClass).removeClass("blur-show");
+        });
+
     });
 });
 /*new WOW().init();*/
